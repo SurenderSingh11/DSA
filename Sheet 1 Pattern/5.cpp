4b46347c38d8d28eadfd0b2a37f7623ae51e9ddb
SurenderSingh11 <srockaswal@gmail.com>
2022-01-12 21:17:15 +0530

    patterns

---
+++ Sheet 1 Pattern/5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n:\n";
+    cin>>n;
+    
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n-i; j++)
+        {
+            cout<<" ";
+        }
+
+        for(int j = 1; j <= i; j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+
+    for(int i = 1; i <= n-1; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout<<" ";
+        }
+        for(int j = n-1; j >= i; j--)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+
+    }
+    
+    return 0;
+}
